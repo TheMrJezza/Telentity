59240d6d21da062369754317068587dc3d962570
Jeremy Bouchier <TheMrJezza@users.noreply.github.com>
2024-04-24 18:11:18 +1000

    Initial commit

---
+++ src/main/java/net/telentity/layer/PassengerGetter.java
@@ -0,0 +1,10 @@
+package net.telentity.layer;
+
+import org.bukkit.entity.Entity;
+import java.util.List;
+
+public interface PassengerGetter {
+
+    List<Entity> getPassengers();
+
+}
---
+++ src/main/java/net/telentity/layer/Refresh1_8_8.java
@@ -0,0 +1,11 @@
+package net.telentity.layer;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+
+public class Refresh1_8_8 {
+
+    public void destroyEntity(Entity entity, Player player) {
+
+    }
+}
